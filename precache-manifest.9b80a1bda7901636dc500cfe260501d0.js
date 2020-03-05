1416953b6f38fa73b0c971eee2ba2a0aa235acbb
Leo Gonsalves <lcgonsalves@wpi.edu>
2020-03-05 03:08:54 -0500

    Updates

---
+++ precache-manifest.9b80a1bda7901636dc500cfe260501d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02d97a3b6d07c45c4b68a6575fff06d",
+    "url": "/WPI-Map-Poll/index.html"
+  },
+  {
+    "revision": "8114104fc0ffc291f412",
+    "url": "/WPI-Map-Poll/static/css/main.03b10330.chunk.css"
+  },
+  {
+    "revision": "ea07c34347df1c55e716",
+    "url": "/WPI-Map-Poll/static/js/2.4c7960e2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/WPI-Map-Poll/static/js/2.4c7960e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8114104fc0ffc291f412",
+    "url": "/WPI-Map-Poll/static/js/main.f8c7c7b5.chunk.js"
+  },
+  {
+    "revision": "f9667670478b0ef86fdb",
+    "url": "/WPI-Map-Poll/static/js/runtime-main.d3e5991d.js"
+  }
+]);
